a2425810d7ee50b842c89a82bbf73dc58a2b0bb4
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-11-01 15:17:53 -0300

    configuracao inicial do servidor

---
+++ back-end/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+
+const app = express();
+
+app.use(express.json());
+
+
+const PORT = 3001;
+
+app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
+
+module.exports = app;
