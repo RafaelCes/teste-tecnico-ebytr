31192abda32617dda929953db950a619a1c9e985
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-11-01 18:50:07 -0300

    cria a funcao que recebe a requisicao para a criacao de uma nova rota

--- back-end/src/controllers/taskController.js
+++ back-end/src/controllers/taskController.js
@@ -4,13 +4,26 @@ const taskService = require('../services/taskService');
 
 const validateRequest = (body) => {
   const { error } = joi.object({
-    name: joi.string().required(),
-    ingredients: joi.string().required(),
-    preparation: joi.string().required(),
+    name: joi.string().max(40).required(),
+    description: joi.string().required(),
+    status: joi.string().required(),
   }).validate(body);
   return error;
 };
 
 const createTask = async (req,res,next) => {
+  const { body } = req;
+  const { userID } = req.user;
 
-}
+  const error = validateRequest(body);
+
+  if (error) return next('Invalid entries. Try again.');
+
+  const response = await taskService.createTask(body, userID)
+
+  res.status(201).json(response);
+};
+
+module.exports = {
+  createTask,
+};
