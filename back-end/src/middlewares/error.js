9b1a4b7a12f3b9a3401e32affd0102c7d7e12f41
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-11-01 17:28:13 -0300

    cria uma rota para realizar login /login

--- back-end/src/middlewares/error.js
+++ back-end/src/middlewares/error.js
@@ -2,8 +2,8 @@ module.exports = (err, _req, res, _next) => {
   const code = {
     'Invalid entries. Try again.': 400,
     'Email already registered': 409,
-    // 'All fields must be filled': 401,
-    // 'Incorrect username or password': 401,
+    'All fields must be filled': 401,
+    'Incorrect username or password': 401,
     // 'jwt malformed': 401,
     // 'recipe not found': 404,
     // 'missing auth token': 401,
