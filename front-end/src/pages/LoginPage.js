0ce30ed40bf0d7a3d2b230b60ef2d8790a17b3f9
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-11-03 11:59:41 -0300

    cria uma nova pagina TaskListPage e redireciona o usuario para ela quando o login e efetuado

---
+++ front-end/src/pages/LoginPage.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Login from '../components/Login';
+import Register from '../components/Register';
+
+export default function LoginPage() {
+  const [register, setRegister] = useState('false');
+  return (
+    <div>
+      <h1> bem vindo!</h1>
+      {register ? <Login /> : <Register /> }
+      <button type="button" onClick={ () => setRegister(!register) }>
+        {register ? 'cadastrar-se' : 'logar-se'}
+      </button>
+    </div>
+  );
+}
